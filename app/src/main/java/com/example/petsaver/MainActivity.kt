e3632addb3d432a72e4dadac5e4465c85e571ae5
felip <felipe.dourados00@gmail.com>
2023-07-31 02:39:28 -0300

    Pagina Home Criada

--- app/src/main/java/com/example/petsaver/MainActivity.kt
+++ app/src/main/java/com/example/petsaver/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.petsaver
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import com.example.petsaver.databinding.ActivityMainBinding
 
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
