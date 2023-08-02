2cb9ff4dad52e44c8d3c48c24a74880c2ccbf957
felip <felipe.dourados00@gmail.com>
2023-08-02 00:13:37 -0300

    Pagina Bem-vindo terminada

--- app/src/main/java/com/example/petsaver/BemVindoFragment.kt
+++ app/src/main/java/com/example/petsaver/BemVindoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.petsaver.databinding.FragmentBemVindoBinding
 
 class BemVindoFragment : Fragment() {
@@ -19,5 +20,13 @@ class BemVindoFragment : Fragment() {
         _binding = FragmentBemVindoBinding.inflate(inflater,container,false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonBemVindoHome.setOnClickListener {
+            binding.buttonBemVindoHome.findNavController().navigate(R.id.action_bemVindoFragment_to_home2)
+        }
+
     }
 }
