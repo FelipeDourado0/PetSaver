955a4229cc00b0a9f0dfe15f42271b7627b1082a
felip <felipe.dourados00@gmail.com>
2023-08-23 21:40:46 -0300

    configuração de animação de troca de tela para a tela "Bem vindo"

--- app/src/main/java/com/example/petsaver/BemVindoFragment.kt
+++ app/src/main/java/com/example/petsaver/BemVindoFragment.kt
@@ -1,6 +1,7 @@
 package com.example.petsaver
 
 import android.os.Bundle
+import android.transition.TransitionInflater
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +14,11 @@ class BemVindoFragment : Fragment() {
     private var _binding : FragmentBemVindoBinding? = null
     private val binding get() = _binding!!
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val inflater = TransitionInflater.from(requireContext())
+        exitTransition = inflater.inflateTransition(R.transition.slide_right)
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
--- app/src/main/java/com/example/petsaver/MainActivity.kt
+++ app/src/main/java/com/example/petsaver/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.input.key.Key.Companion.Home
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
