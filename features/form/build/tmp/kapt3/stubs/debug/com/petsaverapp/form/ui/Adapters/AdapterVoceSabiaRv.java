aaf0029a37a6806d70732d017e137fc9ab139f30
felip <felipe.dourados00@gmail.com>
2023-11-06 07:43:51 -0300

    refatoração build

--- features/form/build/tmp/kapt3/stubs/debug/com/petsaverapp/form/ui/Adapters/AdapterVoceSabiaRv.java
+++ features/form/build/tmp/kapt3/stubs/debug/com/petsaverapp/form/ui/Adapters/AdapterVoceSabiaRv.java
@@ -1,4 +1,4 @@
-package com.petsaverapp.form.ui.Adapters;
+package com.petsaverapp.form.ui.adapters;
 
 import android.content.Context;
 import android.graphics.RenderEffect;
@@ -16,8 +16,8 @@ import com.petsaverapp.core.data.database.model.MateriaDomain;
 import com.petsaverapp.form.R;
 import com.petsaverapp.form.ui.dataEntities.Materia;
 
-@kotlin.Metadata(mv = {1, 8, 0}, k = 1, xi = 48, d1 = {"\u0000@\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0010\u0002\n\u0002\b\u0005\n\u0002\u0010\b\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0004\u0018\u00002\f\u0012\b\u0012\u00060\u0002R\u00020\u00000\u0001:\u0002\u001a\u001bB\u001b\u0012\u0006\u0010\u0003\u001a\u00020\u0004\u0012\f\u0010\u0005\u001a\b\u0012\u0004\u0012\u00020\u00070\u0006\u00a2\u0006\u0002\u0010\bJ\b\u0010\u0011\u001a\u00020\u0012H\u0016J\u001c\u0010\u0013\u001a\u00020\f2\n\u0010\u0014\u001a\u00060\u0002R\u00020\u00002\u0006\u0010\u0015\u001a\u00020\u0012H\u0016J\u001c\u0010\u0016\u001a\u00060\u0002R\u00020\u00002\u0006\u0010\u0017\u001a\u00020\u00182\u0006\u0010\u0019\u001a\u00020\u0012H\u0016R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0014\u0010\u0005\u001a\b\u0012\u0004\u0012\u00020\u00070\u0006X\u0082\u0004\u00a2\u0006\u0002\n\u0000R(\u0010\t\u001a\u0010\u0012\u0004\u0012\u00020\u000b\u0012\u0004\u0012\u00020\f\u0018\u00010\nX\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\r\u0010\u000e\"\u0004\b\u000f\u0010\u0010\u00a8\u0006\u001c"}, d2 = {"Lcom/petsaverapp/form/ui/Adapters/AdapterVoceSabiaRv;", "Landroidx/recyclerview/widget/RecyclerView$Adapter;", "Lcom/petsaverapp/form/ui/Adapters/AdapterVoceSabiaRv$MateriaViewHolder;", "context", "Landroid/content/Context;", "items", "", "Lcom/petsaverapp/core/data/database/model/MateriaDomain;", "(Landroid/content/Context;Ljava/util/List;)V", "onItemCLick", "Lkotlin/Function1;", "Lcom/petsaverapp/form/ui/dataEntities/Materia;", "", "getOnItemCLick", "()Lkotlin/jvm/functions/Function1;", "setOnItemCLick", "(Lkotlin/jvm/functions/Function1;)V", "getItemCount", "", "onBindViewHolder", "holder", "position", "onCreateViewHolder", "parent", "Landroid/view/ViewGroup;", "viewType", "MateriaClickListenner", "MateriaViewHolder", "form_debug"})
-public final class AdapterVoceSabiaRv extends androidx.recyclerview.widget.RecyclerView.Adapter<com.petsaverapp.form.ui.Adapters.AdapterVoceSabiaRv.MateriaViewHolder> {
+@kotlin.Metadata(mv = {1, 8, 0}, k = 1, xi = 48, d1 = {"\u0000@\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0010\u0002\n\u0002\b\u0005\n\u0002\u0010\b\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0004\u0018\u00002\f\u0012\b\u0012\u00060\u0002R\u00020\u00000\u0001:\u0002\u001a\u001bB\u001b\u0012\u0006\u0010\u0003\u001a\u00020\u0004\u0012\f\u0010\u0005\u001a\b\u0012\u0004\u0012\u00020\u00070\u0006\u00a2\u0006\u0002\u0010\bJ\b\u0010\u0011\u001a\u00020\u0012H\u0016J\u001c\u0010\u0013\u001a\u00020\f2\n\u0010\u0014\u001a\u00060\u0002R\u00020\u00002\u0006\u0010\u0015\u001a\u00020\u0012H\u0016J\u001c\u0010\u0016\u001a\u00060\u0002R\u00020\u00002\u0006\u0010\u0017\u001a\u00020\u00182\u0006\u0010\u0019\u001a\u00020\u0012H\u0016R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0014\u0010\u0005\u001a\b\u0012\u0004\u0012\u00020\u00070\u0006X\u0082\u0004\u00a2\u0006\u0002\n\u0000R(\u0010\t\u001a\u0010\u0012\u0004\u0012\u00020\u000b\u0012\u0004\u0012\u00020\f\u0018\u00010\nX\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\r\u0010\u000e\"\u0004\b\u000f\u0010\u0010\u00a8\u0006\u001c"}, d2 = {"Lcom/petsaverapp/form/ui/adapters/AdapterVoceSabiaRv;", "Landroidx/recyclerview/widget/RecyclerView$Adapter;", "Lcom/petsaverapp/form/ui/adapters/AdapterVoceSabiaRv$MateriaViewHolder;", "context", "Landroid/content/Context;", "items", "", "Lcom/petsaverapp/core/data/database/model/MateriaDomain;", "(Landroid/content/Context;Ljava/util/List;)V", "onItemCLick", "Lkotlin/Function1;", "Lcom/petsaverapp/form/ui/dataEntities/Materia;", "", "getOnItemCLick", "()Lkotlin/jvm/functions/Function1;", "setOnItemCLick", "(Lkotlin/jvm/functions/Function1;)V", "getItemCount", "", "onBindViewHolder", "holder", "position", "onCreateViewHolder", "parent", "Landroid/view/ViewGroup;", "viewType", "MateriaClickListenner", "MateriaViewHolder", "form_debug"})
+public final class AdapterVoceSabiaRv extends androidx.recyclerview.widget.RecyclerView.Adapter<com.petsaverapp.form.ui.adapters.AdapterVoceSabiaRv.MateriaViewHolder> {
     @org.jetbrains.annotations.NotNull
     private final android.content.Context context = null;
     @org.jetbrains.annotations.NotNull
@@ -42,7 +42,7 @@ public final class AdapterVoceSabiaRv extends androidx.recyclerview.widget.Recyc
     
     @java.lang.Override
     @org.jetbrains.annotations.NotNull
-    public com.petsaverapp.form.ui.Adapters.AdapterVoceSabiaRv.MateriaViewHolder onCreateViewHolder(@org.jetbrains.annotations.NotNull
+    public com.petsaverapp.form.ui.adapters.AdapterVoceSabiaRv.MateriaViewHolder onCreateViewHolder(@org.jetbrains.annotations.NotNull
     android.view.ViewGroup parent, int viewType) {
         return null;
     }
@@ -54,18 +54,18 @@ public final class AdapterVoceSabiaRv extends androidx.recyclerview.widget.Recyc
     
     @java.lang.Override
     public void onBindViewHolder(@org.jetbrains.annotations.NotNull
-    com.petsaverapp.form.ui.Adapters.AdapterVoceSabiaRv.MateriaViewHolder holder, int position) {
+    com.petsaverapp.form.ui.adapters.AdapterVoceSabiaRv.MateriaViewHolder holder, int position) {
     }
     
-    @kotlin.Metadata(mv = {1, 8, 0}, k = 1, xi = 48, d1 = {"\u0000\u0016\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\b\n\u0000\bf\u0018\u00002\u00020\u0001J\u0010\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u0005H&\u00a8\u0006\u0006"}, d2 = {"Lcom/petsaverapp/form/ui/Adapters/AdapterVoceSabiaRv$MateriaClickListenner;", "", "onMateriaClick", "", "position", "", "form_debug"})
+    @kotlin.Metadata(mv = {1, 8, 0}, k = 1, xi = 48, d1 = {"\u0000\u0016\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\b\n\u0000\bf\u0018\u00002\u00020\u0001J\u0010\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u0005H&\u00a8\u0006\u0006"}, d2 = {"Lcom/petsaverapp/form/ui/adapters/AdapterVoceSabiaRv$MateriaClickListenner;", "", "onMateriaClick", "", "position", "", "form_debug"})
     public static abstract interface MateriaClickListenner {
         
         public abstract void onMateriaClick(int position);
     }
     
-    @kotlin.Metadata(mv = {1, 8, 0}, k = 1, xi = 48, d1 = {"\u00002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0007\n\u0002\u0018\u0002\n\u0002\b\u0005\b\u0086\u0004\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004R\u0011\u0010\u0005\u001a\u00020\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0007\u0010\bR\u0011\u0010\t\u001a\u00020\n\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000b\u0010\fR\u0011\u0010\r\u001a\u00020\n\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000e\u0010\fR\u001a\u0010\u000f\u001a\u00020\u0010X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0011\u0010\u0012\"\u0004\b\u0013\u0010\u0014R\u0011\u0010\u0015\u001a\u00020\n\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0016\u0010\fR\u0011\u0010\u0017\u001a\u00020\u0018\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0019\u0010\u001aR\u0011\u0010\u001b\u001a\u00020\u0018\u00a2\u0006\b\n\u0000\u001a\u0004\b\u001c\u0010\u001a\u00a8\u0006\u001d"}, d2 = {"Lcom/petsaverapp/form/ui/Adapters/AdapterVoceSabiaRv$MateriaViewHolder;", "Landroidx/recyclerview/widget/RecyclerView$ViewHolder;", "itemView", "Landroid/view/View;", "(Lcom/petsaverapp/form/ui/Adapters/AdapterVoceSabiaRv;Landroid/view/View;)V", "cardView", "Landroidx/cardview/widget/CardView;", "getCardView", "()Landroidx/cardview/widget/CardView;", "detalheFundo", "Landroid/widget/ImageView;", "getDetalheFundo", "()Landroid/widget/ImageView;", "imageBackground", "getImageBackground", "onMateriaListener", "Lcom/petsaverapp/form/ui/Adapters/AdapterVoceSabiaRv$MateriaClickListenner;", "getOnMateriaListener", "()Lcom/petsaverapp/form/ui/Adapters/AdapterVoceSabiaRv$MateriaClickListenner;", "setOnMateriaListener", "(Lcom/petsaverapp/form/ui/Adapters/AdapterVoceSabiaRv$MateriaClickListenner;)V", "photoPerfil", "getPhotoPerfil", "subTitleItem", "Landroid/widget/TextView;", "getSubTitleItem", "()Landroid/widget/TextView;", "titleItem", "getTitleItem", "form_debug"})
+    @kotlin.Metadata(mv = {1, 8, 0}, k = 1, xi = 48, d1 = {"\u00002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0007\n\u0002\u0018\u0002\n\u0002\b\u0005\b\u0086\u0004\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004R\u0011\u0010\u0005\u001a\u00020\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0007\u0010\bR\u0011\u0010\t\u001a\u00020\n\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000b\u0010\fR\u0011\u0010\r\u001a\u00020\n\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000e\u0010\fR\u001a\u0010\u000f\u001a\u00020\u0010X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0011\u0010\u0012\"\u0004\b\u0013\u0010\u0014R\u0011\u0010\u0015\u001a\u00020\n\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0016\u0010\fR\u0011\u0010\u0017\u001a\u00020\u0018\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0019\u0010\u001aR\u0011\u0010\u001b\u001a\u00020\u0018\u00a2\u0006\b\n\u0000\u001a\u0004\b\u001c\u0010\u001a\u00a8\u0006\u001d"}, d2 = {"Lcom/petsaverapp/form/ui/adapters/AdapterVoceSabiaRv$MateriaViewHolder;", "Landroidx/recyclerview/widget/RecyclerView$ViewHolder;", "itemView", "Landroid/view/View;", "(Lcom/petsaverapp/form/ui/adapters/AdapterVoceSabiaRv;Landroid/view/View;)V", "cardView", "Landroidx/cardview/widget/CardView;", "getCardView", "()Landroidx/cardview/widget/CardView;", "detalheFundo", "Landroid/widget/ImageView;", "getDetalheFundo", "()Landroid/widget/ImageView;", "imageBackground", "getImageBackground", "onMateriaListener", "Lcom/petsaverapp/form/ui/adapters/AdapterVoceSabiaRv$MateriaClickListenner;", "getOnMateriaListener", "()Lcom/petsaverapp/form/ui/adapters/AdapterVoceSabiaRv$MateriaClickListenner;", "setOnMateriaListener", "(Lcom/petsaverapp/form/ui/adapters/AdapterVoceSabiaRv$MateriaClickListenner;)V", "photoPerfil", "getPhotoPerfil", "subTitleItem", "Landroid/widget/TextView;", "getSubTitleItem", "()Landroid/widget/TextView;", "titleItem", "getTitleItem", "form_debug"})
     public final class MateriaViewHolder extends androidx.recyclerview.widget.RecyclerView.ViewHolder {
-        public com.petsaverapp.form.ui.Adapters.AdapterVoceSabiaRv.MateriaClickListenner onMateriaListener;
+        public com.petsaverapp.form.ui.adapters.AdapterVoceSabiaRv.MateriaClickListenner onMateriaListener;
         @org.jetbrains.annotations.NotNull
         private final android.widget.ImageView photoPerfil = null;
         @org.jetbrains.annotations.NotNull
@@ -85,12 +85,12 @@ public final class AdapterVoceSabiaRv extends androidx.recyclerview.widget.Recyc
         }
         
         @org.jetbrains.annotations.NotNull
-        public final com.petsaverapp.form.ui.Adapters.AdapterVoceSabiaRv.MateriaClickListenner getOnMateriaListener() {
+        public final com.petsaverapp.form.ui.adapters.AdapterVoceSabiaRv.MateriaClickListenner getOnMateriaListener() {
             return null;
         }
         
         public final void setOnMateriaListener(@org.jetbrains.annotations.NotNull
-        com.petsaverapp.form.ui.Adapters.AdapterVoceSabiaRv.MateriaClickListenner p0) {
+        com.petsaverapp.form.ui.adapters.AdapterVoceSabiaRv.MateriaClickListenner p0) {
         }
         
         @org.jetbrains.annotations.NotNull
